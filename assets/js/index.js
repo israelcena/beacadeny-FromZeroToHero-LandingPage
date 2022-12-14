a7121767ac14677c22ffa19505c6b5142ba11582
Israel Cena <israelsjm@gmail.com>
2022-12-14 16:56:05 -0300

    include table content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// modal js
 const openBtn1 = document.getElementById("openModal--1");
 const closeBtn1 = document.getElementById("closeModal--1");
 const openBtn2 = document.getElementById("openModal--2");
@@ -32,6 +33,29 @@ closeBtn3.addEventListener("click", () => {
   modal3.classList.remove("open");
 });
 
-import scrollPage from "./components/scroll.js";
-
-window.addEventListener('focus', scrollPage());
+// testimonials__table
+const trs = document.querySelectorAll(".testimonials__table--row")
+trs.forEach(
+  tr => {
+    fetch(`https://dev.to/api/users/${tr.rowIndex}`)
+      .then(res => res.json())
+      .then(data => {
+        const td1 = document.createElement("td")
+        const td2 = document.createElement("td")
+        const td3 = document.createElement("td")
+        const td4 = document.createElement("td")
+        const td5 = document.createElement("td")
+        td1.innerHTML = data.id
+        td2.innerHTML = `<img src=${data.profile_image} />`
+        td3.innerHTML = data.name
+        td4.innerHTML = data.summary
+        td5.innerHTML = data.username
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+        tr.appendChild(td4)
+        tr.appendChild(td5)
+      })
+      .catch(err => console.error(err))
+  }
+)
